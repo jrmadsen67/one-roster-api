2b6029d318b735cd99bbe7534eed10f0e0255cd7
Jeff Madsen <jrmadsen67@gmail.com>
2021-03-21 10:22:37 +0900

    WIP

--- src/OneRosterApiServiceProvider.php
+++ src/OneRosterApiServiceProvider.php
@@ -7,44 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class OneRosterApiServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap the application services.
-     */
-    public function boot()
-    {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'one-roster-api');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'one-roster-api');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('one-roster-api.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/one-roster-api'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/one-roster-api'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/one-roster-api'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
-        }
-    }
-
-    /**
      * Register the application services.
      */
     public function register()
@@ -52,9 +14,11 @@ class OneRosterApiServiceProvider extends ServiceProvider
         // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'one-roster-api');
 
-        // Register the main class to use with the facade
-        $this->app->singleton('one-roster-api', function () {
-            return new OneRosterApi;
+        $this->app->bind(OneRosterApi::class, function () {
+            // if we're not on production then swap out the real autopilot for the dummy one
+            return in_array($this->app->environment(), ['production', 'local']) ?
+                app(OneRosterApiClient::class) :
+                app(OneRosterApiMock::class);
         });
     }
 }
