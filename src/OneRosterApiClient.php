b8211f94fe07e7da00e0748c088d1c440119e066
Jeff Madsen <jrmadsen67@gmail.com>
2021-03-22 19:28:26 +0900

    a few clean up tasks

--- src/OneRosterApiClient.php
+++ src/OneRosterApiClient.php
@@ -50,116 +50,116 @@ class OneRosterApiClient implements OneRosterApi
     public function getAllAcademicSessions($offset=0, $limit=100, $filter = 'status="active"'){
         $queryString = $this->createParameterString($offset,$limit);
         // @TODO: error handle $response
-        return $response = $this->client->get('/academicSessions' . $queryString);
+        return $response = $this->client->get('academicSessions' . $queryString);
     }
 
     public function getAcademicSession($session){
         // @TODO: error handle $response
-        return $response = $this->client->get('/academicSessions/' . $session);
+        return $response = $this->client->get('academicSessions/' . $session);
     }
 
     public function getAllClasses($offset=0, $limit=100, $filter = 'status="active"'){
         $queryString = $this->createParameterString($offset,$limit);
         // @TODO: error handle $response
-        return $response = $this->client->get('/classes' . $queryString);
+        return $response = $this->client->get('classes' . $queryString);
     }
 
     public function getClass($class){
         // @TODO: error handle $response
-        return $response = $this->client->get('/classes/' . $class);
+        return $response = $this->client->get('classes/' . $class);
     }
 
     public function getAllCourses($offset=0, $limit=100, $filter = 'status="active"'){
         $queryString = $this->createParameterString($offset,$limit);
         // @TODO: error handle $response
-        return $response = $this->client->get('/courses' . $queryString);
+        return $response = $this->client->get('courses' . $queryString);
     }
 
     public function getCourse($course){
         // @TODO: error handle $response
-        return $response = $this->client->get('/courses/' . $course);
+        return $response = $this->client->get('courses/' . $course);
     }
 
     public function getAllEnrollments($offset=0, $limit=100, $filter = 'status="active"'){
         $queryString = $this->createParameterString($offset,$limit);
         // @TODO: error handle $response
-        return $response = $this->client->get('/enrollments' . $queryString);
+        return $response = $this->client->get('enrollments' . $queryString);
     }
 
     public function getEnrollment($enrollment){
         // @TODO: error handle $response
-        return $response = $this->client->get('/enrollments/' . $enrollment);
+        return $response = $this->client->get('enrollments/' . $enrollment);
     }
 
     public function getAllGradingPeriods($offset=0, $limit=100, $filter = 'status="active"'){
         $queryString = $this->createParameterString($offset,$limit);
         // @TODO: error handle $response
-        return $response = $this->client->get('/gradingPeriods' . $queryString);
+        return $response = $this->client->get('gradingPeriods' . $queryString);
     }
 
     public function getGradingPeriod($gradingPeriod){
         // @TODO: error handle $response
-        return $response = $this->client->get('/gradingPeriods/' . $gradingPeriod);
+        return $response = $this->client->get('gradingPeriods/' . $gradingPeriod);
     }
 
     public function getAllOrgs($offset=0, $limit=100, $filter = 'status="active"'){
         $queryString = $this->createParameterString($offset,$limit);
         // @TODO: error handle $response
-        return $response = $this->client->get('/orgs' . $queryString);
+        return $response = $this->client->get('orgs' . $queryString);
     }
 
     public function getAllSchools($offset=0, $limit=100, $filter = 'status="active"'){
         $queryString = $this->createParameterString($offset,$limit);
         // @TODO: error handle $response
-        return $response = $this->client->get('/schools' . $queryString);
+        return $response = $this->client->get('schools' . $queryString);
     }
 
     public function getSchool($school){
         // @TODO: error handle $response
-        return $response = $this->client->get('/schools/' . $school);
+        return $response = $this->client->get('schools/' . $school);
     }
 
     public function getAllStudents($offset=0, $limit=100, $filter = 'status="active"'){
         $queryString = $this->createParameterString($offset,$limit);
         // @TODO: error handle $response
-        return $response = $this->client->get('/students' . $queryString);
+        return $response = $this->client->get('students' . $queryString);
     }
 
     public function getStudent($student){
         // @TODO: error handle $response
-        return $response = $this->client->get('/students/' . $student);
+        return $response = $this->client->get('students/' . $student);
     }
 
     public function getAllTeachers($offset=0, $limit=100, $filter = 'status="active"'){
         $queryString = $this->createParameterString($offset,$limit);
         // @TODO: error handle $response
-        return $response = $this->client->get('/teachers' . $queryString);
+        return $response = $this->client->get('teachers' . $queryString);
     }
 
     public function getTeacher($teacher){
         // @TODO: error handle $response
-        return $response = $this->client->get('/teachers/' . $teacher);
+        return $response = $this->client->get('teachers/' . $teacher);
     }
 
     public function getAllTerms($offset=0, $limit=100, $filter = 'status="active"'){
         $queryString = $this->createParameterString($offset,$limit);
         // @TODO: error handle $response
-        return $response = $this->client->get('/terms' . $queryString);
+        return $response = $this->client->get('terms' . $queryString);
     }
 
     public function getTerm($term){
         // @TODO: error handle $response
-        return $response = $this->client->get('/terms/' . $term);
+        return $response = $this->client->get('terms/' . $term);
     }
 
     public function getAllUsers($offset=0, $limit=100, $filter = 'status="active"'){
         $queryString = $this->createParameterString($offset,$limit);
         // @TODO: error handle $response
-        return $response = $this->client->get('/users' . $queryString);
+        return $response = $this->client->get('users' . $queryString);
     }
 
     public function getUser($user){
         // @TODO: error handle $response
-        return $response = $this->client->get('/users/' . $user);
+        return $response = $this->client->get('users/' . $user);
     }
 }
